b2629b214662c878eb0b3188692ba205bf817c21
GameDev46 <76485006+GameDev46@users.noreply.github.com>
2023-09-30 14:39:28 +0100

    Add files via upload

---
+++ neuralNetwork.js
@@ -0,0 +1,369 @@
+/*
+
+ _____                         ______                 ___   ____ 
+|  __ \                        |  _  \               /   | / ___|
+| |  \/  __ _  _ __ ___    ___ | | | |  ___ __   __ / /| |/ /___ 
+| | __  / _` || '_ ` _ \  / _ \| | | | / _ \\ \ / // /_| || ___ \
+| |_\ \| (_| || | | | | ||  __/| |/ / |  __/ \ V / \___  || \_/ |
+ \____/ \__,_||_| |_| |_| \___||___/   \___|  \_/      |_/\_____/
+
+
+*/
+
+
+class NeuralNetwork {
+	layers = [];
+	weights = [];
+	biases = [];
+	neuralLayers = [];
+	activationFunction = [];
+	decimalPlaces = 5;
+	learningRate = 0.1;
+
+	constructor(options) {
+		let inputs = options.inputs;
+		let layers = options.layers;
+		let outputs = options.outputs;
+
+		this.learningRate = options.learningRate;
+
+		this.neuralLayers = layers;
+
+		this.decimalPlaces = options.decimalPlaces || 5;
+
+		this.layers = new Array(layers.length + 2).fill(0);
+		this.biases = new Array(layers.length + 1).fill(0);
+
+		this.layers[0] = new Array(inputs).fill(0);
+
+		for (let i = 0; i < layers.length; i++) {
+			this.layers[i + 1] = new Array(layers[i]).fill(0);
+		}
+
+		this.layers[this.layers.length - 1] = new Array(outputs).fill(0);
+
+		this.activationFunction = new Array(layers.length + 2).fill(this.relU);
+
+		this.setupWeights();
+		this.setupBiases();
+	}
+
+	predict(data) {
+
+		let count = Math.min(data.length, this.layers[0].length);
+
+		for (let i = 0; i < count; i++) {
+			this.layers[0][i] = this.round(data[i]);
+		}
+
+		this.calculateOutputs();
+
+		return this.layers[this.layers.length - 1];
+	}
+
+	getOutput() {
+		return this.layers[this.layers.length - 1];
+	}
+
+	setupWeights() {
+
+		// Weight is read where first level is layer, second level is node and third level is previous nodes
+
+		let weights = [];
+
+		// Setup layer weights
+		for (let x = 1; x < this.layers.length; x++) {
+
+			let weightsToAdd = [];
+
+			for (let y = 0; y < this.layers[x].length; y++) {
+
+				let nodeWeightData = [];
+
+				for (let i = 0; i < this.layers[x - 1].length; i++) {
+
+					nodeWeightData.push(this.round((Math.random() * 2) - 1));
+
+				}
+
+				weightsToAdd.push(nodeWeightData);
+
+			}
+
+			weights.push(weightsToAdd);
+
+		}
+
+		this.weights = weights;
+
+	}
+
+	setupBiases() {
+
+		this.biases = [];
+
+		for (let x = 1; x < this.layers.length; x++) {
+
+			let biasData = [];
+
+			for (let y = 0; y < this.layers[x].length; y++) {
+				biasData.push(this.round((Math.random() * 2) - 1));
+			}
+
+			this.biases.push(biasData)
+		}
+
+	}
+
+	calculateOutputs() {
+
+		for (let x = 1; x < this.layers.length; x++) {
+
+			for (let y = 0; y < this.layers[x].length; y++) {
+
+				let data = this.layers[x - 1];
+
+				for (let i = 0; i < data.length; i++) {
+					this.layers[x][y] += this.round(data[i] * this.weights[x - 1][y][i]);
+				}
+
+				// Activation function
+
+				this.layers[x][y] = this.round(this.activationFunction[x - 1](this.layers[x][y] + this.biases[x - 1][y]));
+
+			}
+		}
+
+	}
+
+	setActivationFunction(types) {
+
+		for (let i = 0; i < Math.min(types.length, this.activationFunction.length); i++) {
+
+			this.activationFunction[i] = types[i];
+
+		}
+
+	}
+
+	// Activation functions (Only sigmoid has a derivative currently)
+
+	relU(num) {
+		return Math.max(0, num);
+	}
+
+	sigmoid(num) {
+		return 1 / (1 + Math.exp(-num))
+	}
+
+	derivativeSigmoid(num) {
+		return this.sigmoid(num) * (1 - this.sigmoid(num));
+	}
+
+	fakeDSigmoid(num) {
+		return num * (1 - num);
+	}
+
+	hyperbolicTangent(num) {
+		return Math.tanh(num);
+	}
+
+	round(num) {
+		let multi = 10 ^ this.decimalPlaces;
+
+		return Math.round(num * multi) / multi;
+	}
+
+	merge(mergeNeuron) {
+
+		// Average together the weights of this neural network and another
+
+		for (let x = 0; x < Math.min(mergeNeuron.weights.length, this.weights.length); x++) {
+
+			for (let y = 0; y < Math.min(mergeNeuron.weights[x].length, this.weights[x].length); y++) {
+
+				for (let i = 0; i < Math.min(mergeNeuron.weights[x][y].length, this.weights[x][y].length); i++) {
+
+					// Merge weights
+					let opts = [this.weights[x][y][i], mergeNeuron.weights[x][y][i]];
+					this.weights[x][y][i] = (opts[0] + opts[1]) / 2;
+
+				}
+
+			}
+
+		}
+
+		for (let x = 0; x < Math.min(mergeNeuron.biases.length, this.biases.length); x++) {
+
+			for (let y = 0; y < Math.min(mergeNeuron.biases[x].length, this.biases[x].length); y++) {
+				let opts = [this.biases[x][y], mergeNeuron.biases[x][y]]
+				this.biases[x][y] = (opts[0] + opts[1]) / 2;
+			}
+
+		}
+
+	}
+
+	mutate(mutationChance, mutationIntensity) {
+
+		// Randomly change the weights in the neural network
+
+		for (let x = 0; x < this.weights.length; x++) {
+
+			for (let y = 0; y < this.weights[x].length; y++) {
+
+				for (let i = 0; i < this.weights[x][y].length; i++) {
+
+					if (Math.random() * 100 < mutationChance) {
+						// Mutate weight
+						this.weights[x][y][i] += (Math.random() * mutationIntensity * 2) - mutationIntensity;
+
+					}
+
+				}
+
+			}
+
+		}
+	}
+
+	copy(mergeNeuron) {
+
+		// Replicate the inputted neuron ( mergeNeuron )
+
+		for (let x = 0; x < Math.min(mergeNeuron.weights.length, this.weights.length); x++) {
+
+			for (let y = 0; y < Math.min(mergeNeuron.weights[x].length, this.weights[x].length); y++) {
+
+				for (let i = 0; i < Math.min(mergeNeuron.weights[x][y].length, this.weights[x][y].length); i++) {
+
+					// Replace weights
+					this.weights[x][y][i] = mergeNeuron.weights[x][y][i];
+
+				}
+
+			}
+
+		}
+
+		for (let x = 0; x < Math.min(mergeNeuron.biases.length, this.biases.length); x++) {
+
+			for (let y = 0; y < Math.min(mergeNeuron.biases[x].length, this.biases[x].length); y++) {
+
+				this.biases[x][y] = mergeNeuron.biases[x][y];
+
+			}
+
+		}
+
+	}
+
+	train(networkInputs, expectedOutputs) {
+
+		// Setup output errors
+
+		let currentOutputs = this.predict(networkInputs);
+
+		let error = [];
+
+		// Calculate the intial output errors ( TARGET - OUTPUT )
+		for (let i = 0; i < Math.min(currentOutputs.length, expectedOutputs.length); i++) {
+			error.push(expectedOutputs[i] - currentOutputs[i]);
+		}
+
+		for (let x = (this.layers.length - 1); x > 0; x--) {
+
+			let nextNodeErrors = [];
+
+			for (let y = 0; y < this.layers[x].length; y++) {
+
+				/*let summedWeights = 0;
+
+				for (let i = 0; i < this.layers[x - 1].length; i++) {
+
+					// Sum up the weights for each neuron
+					summedWeights += this.weights[x - 1][y][i];
+
+				}*/
+
+				// Calculate error of the node
+
+				let nodeError = 0;
+
+				if (x + 1 < this.layers.length) {
+
+					// Sum all previous errors multiplied with the weights
+					for (let n = 0; n < this.layers[x + 1].length; n++) {
+
+						let forwardLayerTotalWeight = 0;
+
+						for (let o = 0; o < this.layers[x].length; o++) {
+							// Calculate the total weight going out of the neuron
+							forwardLayerTotalWeight += this.weights[x][n][o];
+						}
+
+						nodeError += error[n] * (this.weights[x][n][y] / forwardLayerTotalWeight);
+					}
+
+				}
+				else {
+
+					nodeError = error[y];
+
+				}
+
+				// Save layers neuron errors ready to be used for the next layer
+				nextNodeErrors[y] = nodeError;
+
+				// Loop over each weight in the layer
+				for (let i = 0; i < this.layers[x - 1].length; i++) {
+					// Change each weight using gradient descent
+
+					// Calulate gradient
+					let gradient = this.fakeDSigmoid(this.layers[x][y]);
+					gradient *= nodeError;
+					gradient *= this.learningRate;
+
+					// Change bias by gradient
+					if (i == 0) this.biases[x - 1][y] += gradient;
+
+					// Calulate weight delta
+					let weightDelta = gradient * this.layers[x - 1][i];
+
+					// Change weight by calculated delta
+					this.weights[x - 1][y][i] += weightDelta;
+
+					// Old weight change (Doesn't use gradient descent + might not work)
+					//this.weights[x - 1][y][i] += (this.weights[x - 1][y][i] / summedWeights) * nodeError;
+
+				}
+
+			}
+
+			error = nextNodeErrors;
+
+		}
+
+	}
+
+	getDNA() {
+
+		let networkDNA = {};
+
+		networkDNA.weights = this.weights;
+		networkDNA.biases = this.biases;
+
+		networkDNA.inputs = this.layers[0].length;
+		networkDNA.layers = this.neuralLayers;
+		networkDNA.outputs = this.layers[this.layers.length - 1].length;
+
+		networkDNA.decimalPlaces = this.decimalPlaces;
+		networkDNA.activationFunction = this.activationFunction;
+
+		return networkDNA;
+	}
+
+}
+
+export { NeuralNetwork };
---
+++ script.js
@@ -0,0 +1,64 @@
+import { NeuralNetwork } from "/neuralNetwork.js";
+
+const presets = {
+	inputs: 2,
+	layers: [16],
+	outputs: 1,
+	learningRate: 0.2,
+	decimalPlaces: 5
+}
+
+let net = new NeuralNetwork(presets);
+net.setActivationFunction([net.sigmoid, net.sigmoid]);
+
+console.log(net);
+
+const trainingData = [{
+	inputs: [0, 0],
+	targets: [0]
+}, {
+	inputs: [1, 1],
+	targets: [0]
+}, {
+	inputs: [1, 0],
+	targets: [1]
+}, {
+	inputs: [0, 1],
+	targets: [1]
+}]
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const pixelWidth = 100;
+const pixelHeight = 100;
+
+const pixelSize = 5;
+
+canvas.width = pixelSize * pixelWidth;
+canvas.height = pixelSize * pixelHeight;
+
+function loop() {
+
+	for (let i = 0; i < 5000; i++) {
+		let data = trainingData[Math.round(Math.random() * (trainingData.length - 1))];
+		net.train(data.inputs, data.targets);
+	}
+
+	for (let x = 0; x < pixelWidth; x++) {
+		for (let y = 0; y < pixelHeight; y++) {
+
+			let guess = net.predict([x / pixelWidth, y / pixelHeight]);
+			let col = guess[0] * 255;
+
+			ctx.fillStyle = "rgb(" + col + "," + col + "," + col + ")";
+			ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+
+		}
+	}
+
+
+	requestAnimationFrame(loop);
+}
+
+loop();
